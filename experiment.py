2f85540f80b3a4786450a8decce03a06455b3727
Saining Li <saining.li@gmail.com>
2012-09-24 22:29:52 -0400

    finished build_experiment

--- experiment.py
+++ experiment.py
@@ -4,19 +4,31 @@ from logger import *
 from packet_listener import *
 import sys
 import os.path
+import random.choice
 
 def build_experiment():
     experiment = []
+
+    # build trials from grating list
     for grating in GRATINGS:
         if (grating=="h"):
             experiment.append(("vStimHorizontal", DURV))
         else:
             experiment.append(("vStimVertical", DURV))
+        experiment.append(DELAY)
         for i in xrange(3):
             experiment.append(("aStim", FREQ, DURA))
             experiment.append(DURAWAIT)
+        experiment.append(calcITIr())
     
     experiment.reverse()
+    return experiment
+
+def calcITIr():
+    """ ITI with jitter
+    """
+    return ITI + random.choice(xrange(-JITTER, JITTER))
+
 def startui(logname):
     experiment = build_experiment()
     logger=Logger(logname)    
